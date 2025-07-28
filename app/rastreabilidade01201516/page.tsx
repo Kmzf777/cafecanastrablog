f1e151b19e006481b28f18bd06a282e833cd2cb3
Kmzf777 <rafaelreisssilva034@gmail.com>
2025-07-28 18:03:50 -0300

    Adiciona rota de redirecionamento para rastreabilidade01201516

---
+++ app/rastreabilidade01201516/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function RastreabilidadePage() {
+  redirect('https://intranet.cerradomineiro.org/index.php/farmId/produtor/653/501')
+} 
