0399573f83f4db55e5ae6fee6d68906990a078d6
Kmzf777 <rafaelreisssilva034@gmail.com>
2026-05-22 15:50:19 -0300

    feat: adiciona Google Tag Manager (GTM-PX8PWW6) no head e body

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,6 +78,9 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PX8PWW6');` }} />
+        {/* End Google Tag Manager */}
         <link rel="icon" href="/logo-canastra.png" />
         <link rel="apple-touch-icon" href="/logo-canastra.png" />
         <link rel="manifest" href="/manifest.json" />
@@ -136,6 +139,9 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className} suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PX8PWW6" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
         <SkipLink />
         <ThemeProvider
           attribute="class"
