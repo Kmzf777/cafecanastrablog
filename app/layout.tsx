a23bca55b5ad6e1b25165c3510a9ece4fa0adf5a
Kmzf777 <rafaelreisssilva034@gmail.com>
2026-05-25 14:28:23 -0300

    feat: adiciona Google Analytics 4 (G-XPL8Y566L6) no head

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,6 +81,11 @@ export default function RootLayout({
         {/* Google Tag Manager */}
         <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PX8PWW6');` }} />
         {/* End Google Tag Manager */}
+
+        {/* Google Analytics 4 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XPL8Y566L6" />
+        <script dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-XPL8Y566L6');` }} />
+        {/* End Google Analytics 4 */}
         <link rel="icon" href="/logo-canastra.png" />
         <link rel="apple-touch-icon" href="/logo-canastra.png" />
         <link rel="manifest" href="/manifest.json" />
